8c0c4eaeb2f98c3ca73710708cc8c52c55cc357d
Marco Antonio <marcoantonio_teodoro@hotmail.com>
2020-03-09 20:30:35 -0300

    Segundo commit

--- src/br/fai/collections/Main.java
+++ src/br/fai/collections/Main.java
@@ -1,5 +1,9 @@
 package br.fai.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -8,9 +12,24 @@ public class Main {
 	app.start();
 	}
 
+	private Scanner ler = new Scanner(System.in);
+	
+	
 	private void start() {
+		
+		List<String> lista = new ArrayList<String>();
+		String valorRecebido = obterDados();
+		System.out.println("Valor recebido foi: "+ valorRecebido);
+	}
+	
+	private String obterDados() {
+		System.out.println("Digite o valor: ");
+		
+		int valor = ler.nextInt();
+		
+		return String.valueOf(valor);
 		
 		
 	}
-
+	
 }
