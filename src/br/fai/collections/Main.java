26c01179e457c6ba6a7ecc0b618b49d028b564bc
Marco Antonio <marcoantonio_teodoro@hotmail.com>
2020-03-09 20:53:00 -0300

    Terceiro commit

--- src/br/fai/collections/Main.java
+++ src/br/fai/collections/Main.java
@@ -8,28 +8,43 @@ public class Main {
 
 	public static void main(String[] args) {
 
-	Main app = new Main();
-	app.start();
+		Main app = new Main();
+		app.start();
 	}
 
 	private Scanner ler = new Scanner(System.in);
-	
-	
+	private final String VALOR_INVALIDO = "VALOR_INVALIDO";
+
 	private void start() {
-		
+
 		List<String> lista = new ArrayList<String>();
-		String valorRecebido = obterDados();
-		System.out.println("Valor recebido foi: "+ valorRecebido);
+
+		while (lista.size() < 5) {
+			String valorRecebido = obterDados();
+			System.out.println("Valor recebido foi: " + valorRecebido);
+			if (valorRecebido.isEmpty() || valorRecebido.equals(VALOR_INVALIDO)) {
+				System.out.println("Valor descartado.");
+			} else {
+
+				lista.add(valorRecebido);
+			}
+		}
+		System.out.println("-----------------");
+		for (String item : lista) {
+			System.out.println("O item da lista e: " + item);
+
+		}
 	}
-	
+
 	private String obterDados() {
-		System.out.println("Digite o valor: ");
-		
-		int valor = ler.nextInt();
-		
-		return String.valueOf(valor);
-		
-		
+		try {
+			System.out.println("Digite o valor: ");
+			int valor = ler.nextInt();
+			return String.valueOf(valor);
+		} catch (Exception e) {
+			ler.next();
+			System.out.println("Valor invalido foi digitado");
+			return VALOR_INVALIDO;
+		}
 	}
-	
 }
